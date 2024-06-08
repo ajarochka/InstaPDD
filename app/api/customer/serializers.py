c3b0851a92256c2ed09dcd51ae6b2975d17fde2b
2023025intelli <surmali_yusupov@mail.ru>
2024-06-08 14:25:10 +0600

    customer api update

--- app/api/customer/serializers.py
+++ app/api/customer/serializers.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+
+from apps.post.choices import PostStatus
 from utils.common import normalize_phone
 from rest_framework import serializers
 
@@ -6,6 +8,19 @@ UserModel = get_user_model()
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    total_posts = serializers.SerializerMethodField(read_only=True)
+    approved_posts = serializers.SerializerMethodField(read_only=True)
+    rejected_posts = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = UserModel
         exclude = ('password',)
+
+    def get_total_posts(self, obj):
+        return obj.posts.count()
+
+    def get_approved_posts(self, obj):
+        return obj.posts.filter(status=PostStatus.APPROVED).count()
+
+    def get_rejected_posts(self, obj):
+        return obj.posts.filter(status=PostStatus.REJECTED).count()
