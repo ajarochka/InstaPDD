71376728d3b84297dbd517f4bfa7c035d3d1f3cd
2023025intelli <surmali_yusupov@mail.ru>
2024-06-11 21:00:15 +0600

    comment create api

--- app/api/comment/views.py
+++ app/api/comment/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status as http_status
 from .serializers import PostCommentSerializer
 from rest_framework.response import Response
 from utils.rest import CustomJsonRenderer
@@ -25,3 +26,20 @@ class PostCommentsListApiView(generics.ListAPIView):
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
+
+
+class PostCommentCreateApi(generics.CreateAPIView):
+    renderer_classes = (CustomJsonRenderer,)
+    serializer_class = PostCommentSerializer
+    queryset = Post.objects.filter(status=PostStatus.APPROVED)
+    lookup_field = 'pk'
+    lookup_url_kwarg = 'post_pk'
+
+    def create(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=user, post=post)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=http_status.HTTP_200_OK, headers=headers)
